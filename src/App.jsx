35ec9693d52b38a0ee1eeb4219daf857cdc158fe
Fewakes <Slojkowski33@gmail.com>
2023-10-18 03:39:17 +0100

    6th Commit
    Created the 'User' State with Redux Toolkit, Created the 'Cart' State, Building the Cart Overview with Redux Selectors, Building the Cart Page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 
+// Create a browser router with the specified routes
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
-      { path: "/", element: <Home /> },
+      { path: '/', element: <Home /> }, // Define the home page
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
-        loader: menuLoader,
+        loader: menuLoader, // Load the menu data
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> }, // Show the cart page
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
-        action: createOrderAction,
+        action: createOrderAction, // Create a new order
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
-        loader: orderLoader,
+        loader: orderLoader, // Load the order data
         errorElement: <Error />,
       },
     ],
